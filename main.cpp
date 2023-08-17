73d14c69e7377f420ed4a25ec1c171be52a51d6e
Eduardo Ulises Martínez Vaca <81331013+EduardoMV@users.noreply.github.com>
2023-08-17 15:28:49 -0600

    Binary inverse mask implementation and OOP applied.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 using namespace cv;
 using namespace std;
 
-int main() {
-
+void Binary() {
     Mat ori = imread("../flower.jpg", IMREAD_GRAYSCALE);
     //Mat ori2 = imread("../pear.jpg", IMREAD_GRAYSCALE);
     Mat msk(ori.rows, ori.cols, CV_8U, Scalar(0));
@@ -20,14 +19,39 @@ int main() {
                 msk.at<uchar>(row,col) = uchar(0);
             }
         }
+
     }
 
     namedWindow("PhotoFrame", WINDOW_NORMAL);
     imshow("PhotoFrame", msk);
     imwrite("Mask.jpg", msk);
     waitKey(0);
+}
 
+void Binary_Inv(){
+    Mat ori = imread("../flower.jpg", IMREAD_GRAYSCALE);
+    Mat msk_inv(ori.rows, ori.cols, CV_8U, Scalar(0));
+
+    for(int row = 0; row < ori.rows; row++){
+        for(int col = 0; col < ori.cols; col++){
+            if(ori.at<uchar>(row, col) > 125){
+                msk_inv.at<uchar>(row,col) = uchar(0);
+            }
+            else{
+                msk_inv.at<uchar>(row,col) = uchar(255);
+            }
+        }
+
+    }
+
+    namedWindow("PhotoFrame", WINDOW_NORMAL);
+    imshow("PhotoFrame", msk_inv);
+    imwrite("Mask_Inv.jpg", msk_inv);
+    waitKey(0);
+}
+
+
+int main() {
+    Binary_Inv();
     return 0;
-
-
 }
