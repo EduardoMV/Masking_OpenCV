0a84a7b5fa0fe948e6c08071f0af46db012ec8fe
Eduardo Ulises Martínez Vaca <81331013+EduardoMV@users.noreply.github.com>
2023-08-17 14:48:45 -0600

    Implementation and mask deploy

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace cv;
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Mat ori = imread("../flower.jpg", IMREAD_GRAYSCALE);
+    Mat msk(ori.rows, ori.cols, CV_8U, Scalar(0));
+
+    for(int row = 0; row < ori.rows; row++){
+        for(int col = 0; col < ori.cols; col++){
+            if(ori.at<uchar>(row, col) > 125){
+                msk.at<uchar>(row,col) = uchar(255);
+            }
+            else{
+                msk.at<uchar>(row,col) = uchar(0);
+            }
+        }
+    }
+
+    namedWindow("PhotoFrame", WINDOW_NORMAL);
+    imshow("PhotoFrame", msk);
+    waitKey(0);
+
     return 0;
+
+
 }
